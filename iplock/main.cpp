7d96cfa9a36701c6ae9d5eb2de6b5adcc356fd1c
AlexisWilke <alexis@m2osw.com>
2019-10-29 02:41:04 -0700

    Made updates so it compiles with the new advgetopt version.
    Commented out ipload for now until I'm ready to really work on it.

--- iplock/main.cpp
+++ iplock/main.cpp
@@ -73,7 +73,7 @@ int main(int argc, char * argv[])
 
         exit(0);
     }
-    catch(advgetopt::getopt_exception_exit const & e)
+    catch(advgetopt::getopt_exit const & e)
     {
         exit(e.code());
     }
