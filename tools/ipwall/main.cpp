99e033571f487a07e915c93f1e66c91b73943f59
AlexisWilke <alexis@m2osw.com>
2026-08-30 08:25:17 -0700

    Actually remove the stack trace call.
    The initialization automatically happens in the snaplogger and it can be
    changed by the end user with the --except-stack-collect command line option.
    The default is "yes". You can turn it off with "no" or get all the function
    names and parameters demangled along line numbers with "complete".

--- tools/ipwall/main.cpp
+++ tools/ipwall/main.cpp
@@ -52,7 +52,6 @@ int main(int argc, char * argv[])
 {
     ed::signal_handler::create_instance();
     libexcept::verify_inherited_files();
-    libexcept::set_collect_stack(libexcept::collect_stack_t::COLLECT_STACK_YES);
 
     try
     {
